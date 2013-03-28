8a6b313bb04f6cb005964d004ca4d68361986dd6
Thomas Castiglione <castiglione@gmail.com>
2013-03-29 00:41:07 +0800

    get out of there cat

--- .worksheet/src/interactive.scala
+++
@@ -1,3 +0,0 @@
-object interactive {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(64); 
-  println("Welcome to the Scala worksheet")}
-}
