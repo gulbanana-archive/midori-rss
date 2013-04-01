8a2cd49405d7e58b34c85a5c1bfb86b5509e57a3
Thomas Castiglione <castiglione@gmail.com>
2013-04-01 17:18:30 +0800

    separated client and backend

---
+++ web/app/controllers/API.scala
@@ -0,0 +1,51 @@
+package controllers
+
+import scala.concurrent._
+import ExecutionContext.Implicits.global
+import play.api._
+import play.api.mvc._
+import org.joda.time._
+import models._
+import dal._
+import actors._
+
+class API extends Controller { this: DAOComponent with ActorComponent with Authenticator => 
+  private val pageSize = 15
+  
+  def index = Action { 
+    Async { 
+      Authenticated { implicit user => 
+        for (
+          items <- paginatedItems(0, pageSize);
+          marked <- markRead(items)
+        ) yield Ok(views.html.index(user,items))
+      }
+    }
+  }
+  
+  def more = Action(parse.json) { request =>
+    request.body.validate[Int].map {
+      case start => Async { 
+        Authenticated { implicit user => 
+          for (
+            items <- paginatedItems(start, pageSize);
+            marked <- markRead(items)
+          ) yield Ok(views.html.items(items))
+        }
+      } 
+    }.recoverTotal { error =>
+      BadRequest("Invalid request body.")
+    }
+  }
+  
+  private def paginatedItems(skip: Int, take: Int)(implicit user: User) = for (feeds <- dao.getSubscribedFeeds(user)) yield feeds
+    .filter(feed => feed.entries.isDefined)
+    .flatMap(feed => feed.entries.get.map(entry => Item(entry, feed, user.subscriptions.filter(sub => sub.feed==feed.url).flatMap(_.entries).contains(entry.link.toString))))
+    .sorted(Ordering.by[Item,Long](item => item.entry.posted.getMillis).reverse)
+    .drop(skip)
+    .take(take)
+    
+  private def markRead(items: Seq[Item])(implicit user: User) = Future
+    .traverse(items.filter(item => !item.read))(item => dao.markRead(user, item))
+    .map(futures => futures.fold(true)(_ && _))
+}
