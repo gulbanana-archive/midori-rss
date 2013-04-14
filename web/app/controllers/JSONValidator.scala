855501e8c761b4eeb9586db78120f63ffa6e5c33
Thomas Castiglione <castiglione@gmail.com>
2013-04-14 13:01:07 +0800

    JSON endpoint DRY framework

---
+++ web/app/controllers/JSONValidator.scala
@@ -0,0 +1,46 @@
+package controllers
+
+import scala.concurrent._
+import play.api.mvc._
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.libs.json._
+
+trait JSONValidator { this: Controller =>
+  //standard Validated supports Async{} bodies; the difference between 
+  //these two is that ValidatedAsync does the actual validation asynchronously 
+  def ValidatedAsync[A](action: A => Future[Result])(implicit reader: Reads[A]): EssentialAction = {
+    Action(parse.json) { implicit request =>
+      Async {
+        request.body.validate[A].fold(
+          valid = { query =>
+            action(query)
+          },
+          invalid = { e => 
+            Future.successful(BadRequest(JsError.toFlatJson(e)).as("application/json"))
+          }
+        )
+      }
+    }
+  }
+    
+  //in this version validation is up-front before any potential async block is dispatched 
+  //to an actor. however, this is probably what we want; nothing about the validation 
+  //itself is nonblocking, and the body parser is similarly synchronous
+  def Validated[A](action: A => Result)(implicit reader: Reads[A]): EssentialAction = {
+    Action(parse.json) { implicit request =>
+      request.body.validate[A].fold(
+        valid = { query =>
+          action(query)
+        },
+        invalid = { e => 
+          BadRequest(JsError.toFlatJson(e)).as("application/json")
+        }
+      )
+    }
+  }
+  
+  //use on a model class to turn it into a json result
+  implicit class Encodable[A : Writes](wrapped: A)  {
+    def asJson = Ok(Json.toJson(wrapped)).as("application/json")
+  }
+}
--- web/app/controllers/api/News.scala
+++ web/app/controllers/api/News.scala
@@ -1,52 +1,24 @@
 package controllers.api
 
 import scala.concurrent._
-import ExecutionContext.Implicits.global
-import play.api._
 import play.api.mvc._
-import org.joda.time._
-import models._
-import dal._
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import controllers._
 import actors._
-import controllers.Authenticator
+import dal._
+import models._
+import models.api._
 
-class News extends Controller { this: DAOComponent with ActorComponent with Authenticator => 
-  private val pageSize = 15
+class News extends Controller { this: DAOComponent with ActorComponent with Authenticator with JSONValidator => 
+  def get = Validated { query: NewsRequest =>
+    Seq[NewsItem]().asJson
+  }
   
-  def get = Action { 
-    Async { 
-      Authenticated { implicit user => 
-        for (
-          items <- paginatedItems(0, pageSize);
-          marked <- markRead(items)
-        ) yield Ok(views.html.index(user,items))
+  def set = Validated { query: MarkRequest => 
+    Async {
+      Authenticated { user:User => 
+        Future.successful(user.asJson)
       }
     }
   }
-  
-  def set = Action(parse.json) { request =>
-    request.body.validate[Int].map {
-      case start => Async { 
-        Authenticated { implicit user => 
-          for (
-            items <- paginatedItems(start, pageSize);
-            marked <- markRead(items)
-          ) yield Ok(views.html.items(items))
-        }
-      } 
-    }.recoverTotal { error =>
-      BadRequest("Invalid request body.")
-    }
-  }
-  
-  private def paginatedItems(skip: Int, take: Int)(implicit user: User) = for (feeds <- dao.getSubscribedFeeds(user)) yield feeds
-    .filter(feed => feed.entries.isDefined)
-    .flatMap(feed => feed.entries.get.map(entry => Item(entry, feed, user.subscriptions.filter(sub => sub.feed==feed.url).flatMap(_.entries).contains(entry.link.toString))))
-    .sorted(Ordering.by[Item,Long](item => item.entry.posted.getMillis).reverse)
-    .drop(skip)
-    .take(take)
-    
-  private def markRead(items: Seq[Item])(implicit user: User) = Future
-    .traverse(items.filter(item => !item.read))(item => dao.markRead(user, item))
-    .map(futures => futures.fold(true)(_ && _))
 }
